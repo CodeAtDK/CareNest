4da1fb89e22514e4ebfed1c54ffe0ae9934fda79
dhruva <khatavkardhruva@gmail.com>
2026-08-15 23:34:51 +0530

    add the updated ui for the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ import org.gradle.kotlin.dsl.annotationProcessor
 
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     id("com.google.gms.google-services")
 }
 
@@ -32,9 +31,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
     }
     buildFeatures{
 
